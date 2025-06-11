164c389c4a535555ee816ce498bafa74bbbc64e5
Dev Italiya <devitaliya22@gmail.com>
2025-06-11 15:21:06 +0530

    some more bug fixes before publish

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'http://localhost:3001'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization']
