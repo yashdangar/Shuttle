19e5ca594aab20dc8ff37241dacca65cc578bb0c
yashdangar <yashdangar123@gmail.com>
2025-06-16 22:11:18 +0530

    before commit

--- server/src/routes/guestRouter.ts
+++ server/src/routes/guestRouter.ts
@@ -1,21 +1,49 @@
-import express from 'express';
-import controller from '../controller/guestController';
-import { guestAuthMiddleware } from '../middleware/authMiddleware';
-import { RequestHandler } from 'express';
+import express from "express";
+import controller from "../controller/guestController";
+import { guestAuthMiddleware } from "../middleware/authMiddleware";
+import { RequestHandler } from "express";
 
 const router = express.Router();
 
-router.get('/', controller.getGuest);
-router.get('/hotels', guestAuthMiddleware as RequestHandler, controller.getHotels);
-router.post('/set-hotel', guestAuthMiddleware as RequestHandler, controller.setHotel as RequestHandler);
-router.get('/get-hotel/:id', guestAuthMiddleware as RequestHandler, controller.getHotel as RequestHandler);
+router.get("/", controller.getGuest);
+router.get(
+  "/hotels",
+  guestAuthMiddleware as RequestHandler,
+  controller.getHotels
+);
+router.post(
+  "/set-hotel",
+  guestAuthMiddleware as RequestHandler,
+  controller.setHotel as RequestHandler
+);
+router.get(
+  "/get-hotel/:id",
+  guestAuthMiddleware as RequestHandler,
+  controller.getHotel as RequestHandler
+);
 
 //location routes
-router.get('/get-locations', guestAuthMiddleware as RequestHandler, controller.getLocations as RequestHandler);
+router.get(
+  "/get-locations",
+  guestAuthMiddleware as RequestHandler,
+  controller.getLocations as RequestHandler
+);
 
 //trip routes
-router.post('/create-trip', guestAuthMiddleware as RequestHandler, controller.createTrip as RequestHandler);
-router.get('/get-trips', guestAuthMiddleware as RequestHandler, controller.getTrips as RequestHandler);
-router.get('/get-trip/:id', guestAuthMiddleware as RequestHandler, controller.getTrip as RequestHandler);
+router.post(
+  "/create-trip",
+  guestAuthMiddleware as RequestHandler,
+  controller.createTrip as RequestHandler
+);
+router.get(
+  "/get-trips",
+  guestAuthMiddleware as RequestHandler,
+  controller.getTrips as RequestHandler
+);
+router.get(
+  "/get-trip/:id",
+  guestAuthMiddleware as RequestHandler,
+  controller.getTrip as RequestHandler
+);
 
-export default router; 
+export default router;
